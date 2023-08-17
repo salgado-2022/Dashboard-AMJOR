73f8ea4cbbc7450d9f82a6405692981a4d798cbb
juandaxx2343 <juandathebass@gmail.com>
2023-08-17 06:42:07 -0500

    cambios

--- src/sections/@dashboard/configuracion/modal/crearte.js
+++ src/sections/@dashboard/configuracion/modal/crearte.js
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Iconify from "../../../../components/iconify";
 import {
   Button,
   Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Checkbox,
   TextField,
   Dialog,
   DialogTitle,
@@ -21,19 +13,10 @@ import {
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-function ConfiFormulario( {onClose} ) {
+function ConfiFormulario({ onClose }) {
   const [rol, setRol] = useState('');
-  const [selectedPermisos, setSelectedPermisos] = useState([]);
-  const [permisos ] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
-  const [errorMessages, setErrorMessages] = useState({ rol: '', permisos: '' });
-  
-
-  useEffect(() => {
-    console.log('entro')
-    fetchData();
-  }, );
-
+  const [errorMessages, setErrorMessages] = useState({ rol: '', general: '' });
 
   const handleOpenModal = () => {
     setModalOpen(true);
@@ -42,8 +25,7 @@ function ConfiFormulario( {onClose} ) {
   const handleCloseModal = () => {
     setModalOpen(false);
     setRol('');
-    setSelectedPermisos([]);
-    setErrorMessages({ rol: '', permisos: '' });
+    setErrorMessages({ rol: '', general: '' });
   };
 
   const validarRolPermiso = () => {
@@ -53,29 +35,23 @@ function ConfiFormulario( {onClose} ) {
       errors.rol = 'Debes ingresar un rol.';
     }
 
-    if (selectedPermisos.length === 0) {
-      errors.permisos = 'Debes seleccionar al menos 1 permiso.';
-    }
-
     if (Object.keys(errors).length === 0) {
       axios
-        .post('http://localhost:4000/api/crearRol', { rol: selectedPermisos })
+        .post('http://localhost:4000/api/crearRol', { rol })
         .then((res) => {
           if (res.data.Status === 'Success') {
             Swal.fire({
-              title: 'Rol y permisos agregados correctamente',
-              text: 'El rol y permisos han sido creados exitosamente.',
+              title: 'Rol agregado correctamente',
+              text: 'El rol ha sido creado exitosamente.',
               icon: 'success',
             }).then(() => {
               onClose();
               handleCloseModal();
-              fetchData();
-         
             });
           } else {
             setErrorMessages({ ...errorMessages, general: 'Error al crear el rol.' });
           }
-          window.location.reload();
+          window.location.reload()
         })
         .catch((err) => {
           console.log(err);
@@ -85,25 +61,6 @@ function ConfiFormulario( {onClose} ) {
       setErrorMessages(errors);
     }
   };
-  
-  const fetchData = () => {
-    axios
-      .get('http://localhost:4000/api/admin/listpermisos')
-      .then((res) => {
-        console.log(res.data); // Verifica la respuesta de la solicitud en la consola
-      })
-      .catch((err) => console.log('Error al obtener permisos', err));
-  };
-
-
-  const handleCheckboxChange = (permisos) => {
-    if (selectedPermisos.includes(permisos)) {
-      setSelectedPermisos(selectedPermisos.filter((item) => item !== permisos));
-    } else {
-      setSelectedPermisos([...selectedPermisos, permisos]);
-    }
-  };
-
 
   return (
     <Container>
@@ -111,13 +68,13 @@ function ConfiFormulario( {onClose} ) {
         variant="contained"
         color="primary"
         onClick={handleOpenModal}
-        style={{  marginBottom: '16px', float: 'right' }}
+        style={{ marginBottom: '16px', float: 'right' }}
         startIcon={<Iconify icon="eva:plus-fill" />}
       >
-        Crear nuevo rol y permiso
+        Crear nuevo rol
       </Button>
       <Dialog open={modalOpen} onClose={handleCloseModal} fullWidth>
-        <DialogTitle>Crear un nuevo rol y permisos</DialogTitle>
+        <DialogTitle>Crear un nuevo rol</DialogTitle>
         <DialogContent dividers>
           <TextField
             label="Nuevo rol"
@@ -129,46 +86,21 @@ function ConfiFormulario( {onClose} ) {
             error={Boolean(errorMessages.rol)}
             helperText={errorMessages.rol}
           />
-          <br /><br />
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Permiso</TableCell>
-                  <TableCell align="center">Seleccionar</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {permisos.map((permisos) => (
-                  <TableRow key={permisos}>
-                    <TableCell>{permisos}</TableCell>
-                    <TableCell align="center">
-                      <Checkbox
-                        checked={selectedPermisos.includes(permisos)}
-                        onChange={() => handleCheckboxChange(permisos)}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
         </DialogContent>
         <DialogActions>
           <Button
-           type="submit"
-           variant="contained"
-           color="primary"
-           id="modRol"
-           fullWidth
-           style={{ marginTop: '8px' }}
-           onClick={() => {
-             validarRolPermiso();
-             handleCloseModal();
-             onClose();
-           }}
+            type="submit"
+            variant="contained"
+            color="primary"
+            id="modRol"
+            fullWidth
+            style={{ marginTop: '8px' }}
+            onClick={() => {
+              validarRolPermiso();
+              handleCloseModal();
+            }}
           >
-            Crear el rol y permiso
+            Crear el rol
           </Button>
           <Button
             variant="contained"
@@ -182,11 +114,6 @@ function ConfiFormulario( {onClose} ) {
           </Button>
         </DialogActions>
       </Dialog>
-      {Boolean(errorMessages.permisos) && (
-        <Alert severity="error" style={{ marginTop: '8px' }}>
-          {errorMessages.permisos}
-        </Alert>
-      )}
       {Boolean(errorMessages.general) && (
         <Alert severity="error" style={{ marginTop: '8px' }}>
           {errorMessages.general}
