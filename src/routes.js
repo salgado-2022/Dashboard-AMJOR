433a4cfc0556274d63bac52aed0b4f0d64bdc73a
salgado-2022 <salgadojuandavid419@gmail.com>
2023-09-10 18:41:41 -0500

    Seguridad al entrar en admin

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import React, {  useState } from 'react';
 import { Navigate, useRoutes } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import DashboardLayout from './layouts/dashboard';
@@ -15,22 +16,43 @@ import { EditAncheta } from './sections/@dashboard/anchetas/editAncheta';
 import { ConfiFormulario } from './sections/@dashboard/configuracion/modal/crearte';
 import SalesPage from './pages/Sales';
 import Profile from './pages/profile';
+import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_AMJOR_LANDING_URL
+const landingURL = process.env.REACT_APP_AMJOR_LANDING_URL
+const apiUrl = process.env.REACT_APP_AMJOR_API_URL
+
+const token = Cookies.get('token');
 
 function checkTokenInCookies() {
   const token = Cookies.get('token');
   return Boolean(token);
 }
+
 function AuthGuard({ children }) {
+  const [data, setData] = useState();
   const isTokenValid = checkTokenInCookies();
-
   if (!isTokenValid) {
-// Si el token no es válido, redirigir al usuario a la página de inicio de sesión
-    return window.location.href = `${apiUrl}`;
+    // Si el token no es válido, redirigir al usuario a la página de inicio de sesión
+    return window.location.href = `${landingURL}`;
+  } else {
+    axios.get(`${apiUrl}/api/validar/admin/acceso/${token}`)
+      .then((res) => {
+        if (res.data.Status === "Ok") {
+          setData('Ok')
+        }else{
+          setData('Acceso Denegado')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
-
+  console.log("data ruta ", data)
+  if (data === "Acceso Denegado") {
+    return window.location.href = `${landingURL}`;
+  }
   return children;
+
 }
 
 export default function Router() {
@@ -48,10 +70,10 @@ export default function Router() {
         { path: 'sales', element: <SalesPage /> },
         { path: 'anchetas/crearancheta', element: <AddAncheta /> },
         { path: 'anchetas/editarancheta', element: <EditAncheta /> },
-        { path: 'confi', element: <ListaConfiguracion />},
-        { path: 'crearte', element: <ConfiFormulario/>}, 
-        { path: 'profile', element: <Profile/>},
-        { path: 'clientes', element: <ParteCliente/>},
+        { path: 'confi', element: <ListaConfiguracion /> },
+        { path: 'crearte', element: <ConfiFormulario /> },
+        { path: 'profile', element: <Profile /> },
+        { path: 'clientes', element: <ParteCliente /> },
       ],
     },
     {
